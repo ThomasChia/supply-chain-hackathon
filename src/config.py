6299854d5f1e79c03e7d4d6ca4d568c3f2c35628
Thomas Chia <tchia97@gmail.com>
2023-05-19 22:05:45 +0100

    Add in first stage of optimisation.

--- src/config.py
+++ src/config.py
@@ -1,27 +1,51 @@
-vendors = [
-    {"name": "Vendor 1", "cost": 10},
-    {"name": "Vendor 2", "cost": 12},
-    {"name": "Vendor 3", "cost": 8},
-    # Add more vendors as needed
-]
+# vendors = [
+#     {"name": "Vendor 1", "cost": 10},
+#     {"name": "Vendor 2", "cost": 12},
+#     {"name": "Vendor 3", "cost": 8},
+#     # Add more vendors as needed
+# ]
 
-warehouses = [
-    {"name": "Warehouse 1", "transport_cost": 5, "storage_cost": 2},
-    {"name": "Warehouse 2", "transport_cost": 4, "storage_cost": 3},
-    # Add more warehouses as needed
-]
+# warehouses = [
+#     {"name": "Warehouse 1", "transport_cost": 5, "storage_cost": 2},
+#     {"name": "Warehouse 2", "transport_cost": 4, "storage_cost": 3},
+#     # Add more warehouses as needed
+# ]
 
-restaurants = [
-    {"name": "Restaurant 1", "transport_cost": 7},
-    {"name": "Restaurant 2", "transport_cost": 6},
+vendors = {
+    "vendor1": {"cost_per_kg": 10, "capacity": 100},
+    "vendor2": {"cost_per_kg": 20, "capacity": 200},
+    "vendor3": {"cost_per_kg": 50, "capacity": 300},
+}
+
+warehouses = {
+    "warehouse1": {"storage_cost": 5, "inventory_capacity": 100},
+    "warehouse2": {"storage_cost": 7, "inventory_capacity": 150},
+    "warehouse3": {"storage_cost": 9, "inventory_capacity": 200}
+}
+
+restaurants = {
+    "restaurant1": {"transport_cost": 7},
+    "restaurant2": {"transport_cost": 6},
     # Add more restaurants as needed
-]
+}
+
+distance_cost = {
+    ("vendor1", "warehouse1"): {"cost": 1},
+    ("vendor1", "warehouse2"): {"cost": 6},
+    ("vendor1", "warehouse3"): {"cost": 9},
+    ("vendor2", "warehouse1"): {"cost": 12},
+    ("vendor2", "warehouse2"): {"cost": 13},
+    ("vendor2", "warehouse3"): {"cost": 10},
+    ("vendor3", "warehouse1"): {"cost": 8},
+    ("vendor3", "warehouse2"): {"cost": 10},
+    ("vendor3", "warehouse3"): {"cost": 9},
+}
 
 # Example distance data (you'll need to provide actual data)
 supplier_to_warehouse_distances = [
-    [10, 15],  # Distance from Supplier 1 to Warehouse 1 and Warehouse 2
-    [12, 13],  # Distance from Supplier 2 to Warehouse 1 and Warehouse 2
-    [8, 10],   # Distance from Supplier 3 to Warehouse 1 and Warehouse 2
+    [10, 15, 20],  # Distance from Supplier 1 to Warehouse 1 and Warehouse 2 and 3
+    [12, 13, 10],  # Distance from Supplier 2 to Warehouse 1 and Warehouse 2 and 3
+    [8, 10, 9],   # Distance from Supplier 3 to Warehouse 1 and Warehouse 2 and 3
 ]
 
 warehouse_to_restaurant_distances = [
--- src/definition.py
+++ src/definition.py
@@ -1,31 +1,34 @@
 from config import *
 from pulp import LpProblem, LpVariable, lpSum, LpMinimize
 
-# Create the optimization problem
+
 problem = LpProblem("SupplyChainOptimization", LpMinimize)
 
-# Define decision variables
-supply = LpVariable.dicts("Supply", ((v, w) for v in vendors for w in warehouses), lowBound=0, cat="Integer")
-warehouse_inventory = LpVariable.dicts("WarehouseInventory", warehouses, lowBound=0, cat="Integer")
-restaurant_demand = LpVariable.dicts("RestaurantDemand", restaurants, lowBound=0, cat="Integer")
+supply = LpVariable.dicts("supply", [(v, w) for v in vendors for w in warehouses], lowBound=0, cat='Continuous')
+distance = LpVariable.dicts("Distance", distance_cost, lowBound=0, cat="Integer")
 
-# Define the objective function
+
 total_cost = (
-    lpSum(supply[v][w] * vendors[v]["cost"] for v in vendors for w in warehouses) +
-    lpSum(warehouse_inventory[w] * warehouses[w]["storage_cost"] for w in warehouses) +
-    lpSum(restaurant_demand[r] * restaurants[r]["transport_cost"] for r in restaurants)
+    lpSum(supply[(v, w)] * vendors[v]['cost_per_kg'] + distance_cost[d]['cost'] for v in vendors for w in warehouses for d in distance_cost)
 )
 problem += total_cost
 
-# Define the supply and demand constraints
 for v in vendors:
-    problem += lpSum(supply[v][w] for w in warehouses) == 1  # Each vendor must supply one warehouse
+    for w in warehouses:
+        problem += lpSum(supply[(v, w)]) <= vendors[v]['capacity']
+
+for d in distance_cost:
+    problem += lpSum(distance[d] for d in distance_cost) <= 1
+
+problem += lpSum(supply[(v, w)] for v in vendors for w in warehouses) >= 500
+problem += lpSum(distance[d] for d in distance_cost) >= 1
+# problem += lpSum(supply[v] for v in vendors) >= 1
 
 for w in warehouses:
-    problem += lpSum(supply[v][w] for v in vendors) <= warehouse_inventory[w]  # Supply cannot exceed warehouse inventory
+    problem += lpSum(supply[(v, w)] for v in vendors) <= warehouses[w]["inventory_capacity"]  # Supply cannot exceed warehouse inventory
 
-for r in restaurants:
-    problem += lpSum(supply[v][w] for v in vendors for w in warehouses) == restaurant_demand[r]  # Restaurant demand must be met
+# for r in restaurants:
+#     problem += lpSum(supply[(v, w)] for v in vendors for w in warehouses) == restaurant_demand[r]  # Restaurant demand must be met
 
 # Define the capacity constraint for each warehouse (if applicable)
 # Add additional constraints as needed for your specific scenario
@@ -41,17 +44,20 @@ problem.solve()
 if problem.status == 1:  # "Optimal" status code
     # Print the optimal solution
     for v in vendors:
-        for w in warehouses:
-            if supply[v][w].varValue > 0:
-                print(f"Supply {supply[v][w].varValue} units from {v} to {w}")
+        if supply[v].varValue > 0:
+            print(f"Supply {supply[v].varValue} units from {v} at a cost of {supply[v].varValue * vendors[v]['cost_per_kg']}.")
 
-    for w in warehouses:
-        if warehouse_inventory[w].varValue > 0:
-            print(f"Warehouse {w} inventory: {warehouse_inventory[w].varValue} units")
+    for d in distance_cost:
+        if distance[d].varValue > 0:
+            print(f"Distance {distance[d].varValue} units from {d} at a cost of {distance[d].varValue * distance_cost[d]['cost']}.")
 
-    for r in restaurants:
-        if restaurant_demand[r].varValue > 0:
-            print(f"Restaurant {r} demand: {restaurant_demand[r].varValue} units")
+    # for w in warehouses:
+    #     if warehouse_inventory[w].varValue > 0:
+    #         print(f"Warehouse {w} inventory: {warehouse_inventory[w].varValue} units")
+
+    # for r in restaurants:
+    #     if restaurant_demand[r].varValue > 0:
+    #         print(f"Restaurant {r} demand: {restaurant_demand[r].varValue} units")
 
     print(f"Total cost: {problem.objective.value()}")
 else:
