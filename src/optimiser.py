d938419e08f79c9d7ac698ee82a695eac6c10cf3
Thomas Chia <tchia97@gmail.com>
2023-05-20 21:43:07 +0100

    Split print statement from solver.

--- src/optimiser.py
+++ src/optimiser.py
@@ -1,10 +1,10 @@
-from flows import SupplierWarehouseCost, WarehouseRestaurantCost
+from flows import Cost, SupplierWarehouseCost, WarehouseRestaurantCost
 from mapper import RouteCostMapper
 from pulp import LpProblem, LpVariable, lpSum, LpMinimize
 from sites import Vendor, Warehouse, Restaurant
 from typing import List
 
-class SupplyChainOptimization:
+class SupplyChainOptimisation:
     def __init__(self,
                  vendors: List[Vendor],
                  warehouses: List[Warehouse], 
@@ -14,10 +14,8 @@ class SupplyChainOptimization:
         self.vendors = vendors
         self.warehouses = warehouses
         self.restaurants = restaurants
-        self.supplier_warehouse_costs = supplier_warehouse_costs
-        self.warehouse_restaurant_costs = warehouse_restaurant_costs
-        self.supplier_warehouse_mapper = RouteCostMapper()
-        self.warehouse_restaurant_mapper = RouteCostMapper()
+        self.supplier_warehouse_mapper = RouteCostMapper(supplier_warehouse_costs)
+        self.warehouse_restaurant_mapper = RouteCostMapper(warehouse_restaurant_costs)
         self.problem = LpProblem("SupplyChainOptimization", LpMinimize)
         self.supply = LpVariable.dicts("supply", [(v.name, w.name) for v in vendors for w in warehouses], lowBound=0, cat='Continuous')
         self.distribution = LpVariable.dicts("distance", [(w.name, r.name) for w in warehouses for r in restaurants], lowBound=0, cat="Integer")
@@ -26,14 +24,13 @@ class SupplyChainOptimization:
         return lpSum(self.supply[(v.name, w.name)] * v.cost_per_kg for v in self.vendors for w in self.warehouses)
     
     def get_supply_to_warehouse_cost(self):
-        return lpSum(self.supply[(v.name, w.name)] * self.supplier_warehouse_costs.cost for v in self.vendors for w in self.warehouses)
-        # return lpSum(self.supply[(v, w)] * distance_cost.cost for v in self.vendors for w in self.warehouses for distance_cost in self.distance_costs if distance_cost.vendor == v and distance_cost.warehouse == w)
+        return lpSum(self.supply[(v.name, w.name)] * self.supplier_warehouse_mapper.mapping[v.name, w.name] for v in self.vendors for w in self.warehouses)
+    
+    def get_warehouse_storage_cost(self):
+        return lpSum(self.supply[(v.name, w.name)] * w.storage_cost_per_kg for v in self.vendors for w in self.warehouses)
     
     def get_warehouse_to_restaurant_cost(self):
-        return lpSum(self.distribution[(w.name, r.name)] * w.storage_cost_per_kg for w in self.warehouses for r in self.restaurants)
-
-    def add_distribution_cost(self, distribution_cost):
-        self.distribution_costs.append(distribution_cost)
+        return lpSum(self.distribution[(w.name, r.name)] * self.warehouse_restaurant_mapper.mapping[w.name, r.name] for w in self.warehouses for r in self.restaurants)
 
     def add_vendor_constraints(self):
         for v in self.vendors:
@@ -60,33 +57,93 @@ class SupplyChainOptimization:
     def add_restaurant_demand_constraint(self, restaurant: Restaurant):
         self.problem += lpSum(self.distribution[(w.name, restaurant.name)] for w in self.warehouses) >= restaurant.restaurant_demand
 
-    def solve(self):
-        total_cost = (
-            lpSum(self.supply[(v.name, w.name)] * v.cost_per_kg + 
-                  self.supply[(v.name, w.name)] * next((dc.cost for dc in self.distance_costs if dc.vendor == v and dc.warehouse == w), 0)
-                  for v in vendors for w in warehouses) +
-            lpSum(self.distribution[(w.name, r.name)] * next((dc.cost for dc in self.distribution_costs if dc.warehouse == w and dc.restaurant == r), 0)
-                  for w in warehouses for r in restaurants)
-        )
-        self.problem += total_cost
-
-        # Solve the optimization problem
-        self.problem.solve()
-
-        # Check the status of the solution
+    def print_results(self):
         if self.problem.status == 1:  # "Optimal" status code
             # Print the optimal solution
-            for v in vendors:
-                for w in warehouses:
+            for v in self.vendors:
+                for w in self.warehouses:
                     if self.supply[(v.name, w.name)].varValue > 0:
                         print(f"Supply {self.supply[(v.name, w.name)].varValue} units from {v.name} at a cost of {self.supply[(v.name, w.name)].varValue * v.cost_per_kg} and delivered to warehouse {w.name}.")
-                        print(f"Transport costs of {self.supply[(v.name, w.name)].varValue * next((dc.cost for dc in self.distance_costs if dc.vendor == v and dc.warehouse == w), 0)} from {v.name} to {w.name}.")
+                        print(f"Transport costs of {self.supply[(v.name, w.name)].varValue * self.supplier_warehouse_mapper.mapping[v.name, w.name]} from {v.name} to {w.name}.")
 
-            for w in warehouses:
-                for r in restaurants:
+            for w in self.warehouses:
+                for r in self.restaurants:
                     if self.distribution[(w.name, r.name)].varValue > 0:
-                        print(f"Distribution {self.distribution[(w.name, r.name)].varValue} units from {w.name} to {r.name} at a cost of {self.distribution[(w.name, r.name)].varValue * next((dc.cost for dc in self.distribution_costs if dc.warehouse == w and dc.restaurant == r), 0)}.")
+                        print(f"Distribution {self.distribution[(w.name, r.name)].varValue} units from {w.name} to {r.name} at a cost of {self.distribution[(w.name, r.name)].varValue * self.warehouse_restaurant_mapper.mapping[w.name, r.name]}.")
 
             print(f"Total cost: {self.problem.objective.value()}")
         else:
             print("No optimal solution found.")
+
+    def solve(self):
+        total_cost = (
+            self.get_supply_cost() + 
+            self.get_supply_to_warehouse_cost() +
+            self.get_warehouse_storage_cost() +
+            self.get_warehouse_to_restaurant_cost()
+        )
+        self.problem += total_cost
+
+        self.add_vendor_constraints()
+        self.add_warehouse_constraints()
+        self.add_restaurant_constraints()
+
+        self.problem.solve()
+
+        self.print_results()
+
+
+if __name__ == "__main__":
+    vendors = [
+        Vendor("Vendor A", 1200, 0.4, 'London'),
+        Vendor("Vendor B", 800, 0.6, 'Paris'),
+        Vendor("Vendor C", 1500, 0.3, 'Berlin'),
+        Vendor("Vendor D", 1000, 0.5, 'Madrid'),
+        Vendor("Vendor E", 900, 0.7, 'Rome'),
+    ]
+
+    warehouses = [
+        Warehouse("Warehouse 1", 2000, 1, 'London'),
+        Warehouse("Warehouse 2", 1500, 2, 'Paris'),
+        Warehouse("Warehouse 3", 3000, 3, 'Berlin'),
+    ]
+
+    restaurants = [
+        Restaurant("Restaurant 1", 800, 'Madrid'),
+        Restaurant("Restaurant 2", 1200, 'Rome'),
+    ]
+
+    supplier_warehouse_costs = [
+        SupplierWarehouseCost((vendors[0].name, warehouses[0].name), 1),
+        SupplierWarehouseCost((vendors[0].name, warehouses[1].name), 2),
+        SupplierWarehouseCost((vendors[0].name, warehouses[2].name), 3),
+        SupplierWarehouseCost((vendors[1].name, warehouses[0].name), 4),
+        SupplierWarehouseCost((vendors[1].name, warehouses[1].name), 5),
+        SupplierWarehouseCost((vendors[1].name, warehouses[2].name), 6),
+        SupplierWarehouseCost((vendors[2].name, warehouses[0].name), 13),
+        SupplierWarehouseCost((vendors[2].name, warehouses[1].name), 2),
+        SupplierWarehouseCost((vendors[2].name, warehouses[2].name), 10),
+        SupplierWarehouseCost((vendors[3].name, warehouses[0].name), 9),
+        SupplierWarehouseCost((vendors[3].name, warehouses[1].name), 7),
+        SupplierWarehouseCost((vendors[3].name, warehouses[2].name), 5),
+        SupplierWarehouseCost((vendors[4].name, warehouses[0].name), 4),
+        SupplierWarehouseCost((vendors[4].name, warehouses[1].name), 2),
+        SupplierWarehouseCost((vendors[4].name, warehouses[2].name), 1)
+    ]
+
+    warehouse_restaurant_costs = [
+        WarehouseRestaurantCost((warehouses[0].name, restaurants[0].name), 1),
+        WarehouseRestaurantCost((warehouses[0].name, restaurants[1].name), 2),
+        WarehouseRestaurantCost((warehouses[1].name, restaurants[0].name), 3),
+        WarehouseRestaurantCost((warehouses[1].name, restaurants[1].name), 4),
+        WarehouseRestaurantCost((warehouses[2].name, restaurants[0].name), 5),
+        WarehouseRestaurantCost((warehouses[2].name, restaurants[1].name), 6),
+    ]
+
+    supply_chain_optimizer = SupplyChainOptimisation(vendors,
+                                                     warehouses,
+                                                     restaurants,
+                                                     supplier_warehouse_costs,
+                                                     warehouse_restaurant_costs)
+    
+    supply_chain_optimizer.solve()
