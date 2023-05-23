1589cac701b8d7e2f4c8d43b163d2ca5a848ece8
Thomas Chia <tkchia@deloitte.co.uk>
2023-05-23 11:47:13 +0100

    Add db reader.

--- src/connectors/connection.py
+++ src/connectors/connection.py
@@ -3,3 +3,27 @@ from  sqlalchemy.engine import Engine
 from sqlalchemy import create_engine
 
 
+class DbConnection(object):
+    _db_name = 'csc'
+
+    def __init__(self, user, password, host, port):
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.cursor = self.connect()
+
+    def connect(self):
+        conn = psycopg2.connect(
+            database=self._db_name,
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port
+            )
+        return conn.cursor()
+    
+    def reader(self, query_string=None):
+        self.cursor.execute(query_string)
+        data = self.cursor.fetchall()
+        return data
