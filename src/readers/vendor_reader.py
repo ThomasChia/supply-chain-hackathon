09ca6d1d169a07acd8e529abe845bac44f529ae2
Thomas Chia <tchia97@gmail.com>
2023-06-03 13:06:11 +0100

    Update vendor query.

--- src/readers/vendor_reader.py
+++ src/readers/vendor_reader.py
@@ -16,19 +16,20 @@ class VendorReader(Reader):
 
     def build_query(self):
         query = f"""
-        SELECT location.farm_id as name,
-                location.company_name as company,
-                location.nearest_city as location,
-                supply.additional_quantity_available_1st_week_kg_ as capacity,
-                supply.additional_quantity_available_per_week_after_1st_week_kg_ as additional_capacity,
-                supply.SLA_period__days_ as sla_period,
-                supply.onboarding_period as onboarding_period,
-                supply.price_per_kg as cost_per_kg,
-                supply.co2_Emissions_per_kg as co2_emissions_per_kg
-        FROM supplier_farms as location
-        LEFT JOIN supplier_farms_supply as supply
-        ON location.farm_id = supply.farm_id
+        SELECT farm.id as name,
+               supplier.name as company,
+               farm.location as location,
+               supplier.add_qty_1st_week_kg as capacity,
+               supplier.add_qty_after_1st_week_kg as additional_capacity,
+               supplier.sla_period_days as sla_period,
+               supplier.onboarding_period as onboarding_period,
+               supplier.price_kg as cost_per_kg,
+               supplier.co2_emissions_per_kg as co2_emissions_per_kg
+        FROM supplier
+        LEFT JOIN farm
+        ON farm.supplier_id = supplier.id
         WHERE location.farm_id != 'nan'
+        AND supplier.is_active = TRUE
         """
         self.query = query
 
