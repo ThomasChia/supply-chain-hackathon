064d597f609fc6b3ecd117685abbbd7ec69979ef
tom-chia <134644686+tom-chia@users.noreply.github.com>
2023-06-06 12:34:42 +0100

    Update sql for capacity per farm rather than supplier.

--- src/readers/vendor_reader.py
+++ src/readers/vendor_reader.py
@@ -15,22 +15,31 @@ class VendorReader(Reader):
         self.read_query()
 
     def build_query(self):
+        # number of farms per supplier
+        # divide the capacity amount for a supplier by the number of farms they have
+
         query = f"""
+        with supplier_farms as (
+            select supplier_id,
+                    count(id) as supplier_farms
+            from farm
+            group by supplier_id
+        )
         SELECT farm.id as id,
-               farm.name as name,
-               supplier.name as company,
-               farm.location as location,
-               supplier.add_qty_1st_week_kg as capacity,
-               supplier.add_qty_after_1st_week_kg as additional_capacity,
-               supplier.sla_period_days as sla_period,
-               supplier.price_kg as cost_per_kg,
-               supplier.co2_emissions_per_kg as co2_emissions_per_kg,
-               farm.lat as lat,
-               farm.long as long
+            farm.name as name,
+            supplier.name as company,
+            farm.location as location,
+            supplier.add_qty_1st_week_kg / supplier_farms as capacity,
+            supplier.add_qty_after_1st_week_kg / supplier_farms as additional_capacity,
+            supplier.sla_period_days as sla_period,
+            supplier.price_kg as cost_per_kg,
+            supplier.co2_emissions_per_kg as co2_emissions_per_kg,
+            farm.lat as lat,
+            farm.long as long
         FROM farm
-        LEFT JOIN supplier
-        ON farm.supplier_id = supplier.id
-        WHERE supplier.is_active = TRUE
+        LEFT JOIN supplier ON farm.supplier_id = supplier.id
+        left join supplier_farms on farm.supplier_id = supplier_farms.supplier_id
+        WHERE supplier.is_active = true
         """
         self.query = query
 
